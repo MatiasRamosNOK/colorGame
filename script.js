be5ea0a6cc2f8a5d9eaa303ecfada1a6d3ea480b
Matias Ramos <48682417+MatiasRamosNOK@users.noreply.github.com>
2020-02-14 16:34:13 -0300

    Changes

--- script.js
+++ script.js
@@ -1,18 +1,151 @@
+//Genero un color aleatorio
+//Entro en modo facil
+//Seteo botones dificultad
+//Seteo evento para boton correcto
+//Seteo evento para boton incorrecto
+var wrapper = document.getElementsByClassName("wrapper")
 
+
+function generarValorAleatorio(maximo){
+    return (Math.floor(Math.random() * (maximo - 0)) + 0);
+}
+
+
+function mostrarSegunDificultad(dificultad){
+    var stringColor = generarColorAleatorio();
+    var wrapper = document.getElementsByClassName("wrapper")
+    if(dificultad=="facil"){
+        wrapper[0].children[0].style.visibility= "visible";
+        wrapper[0].children[1].style.visibility = "visible";
+        wrapper[0].children[2].style.visibility = "visible";
+        wrapper[0].children[3].style.visibility= "hidden";
+        wrapper[0].children[4].style.visibility = "hidden";
+        wrapper[0].children[5].style.visibility = "hidden";
+    }
+    else{
+        wrapper[0].children[0].style.visibility= "visible";
+        wrapper[0].children[1].style.visibility = "visible";
+        wrapper[0].children[2].style.visibility = "visible";
+        wrapper[0].children[3].style.visibility = "visible";
+        wrapper[0].children[4].style.visibility = "visible";
+        wrapper[0].children[5].style.visibility = "visible";
+    }
+
+    setearColorTexto(stringColor);
+    setearColoresCuadrados(dificultad,stringColor);
+    
+    
+}
+
+function setearEventoIncorrecto(posicion,dificultad){
+    var wrapper = document.getElementsByClassName("wrapper")
+    var maximo;
+    if(dificultad=="facil"){
+        maximo = 3;
+    }
+    else{
+        maximo = 6;
+    }
+
+    for(var i = 0;i<maximo;i++){
+            wrapper[0].children[i].addEventListener("click",function () {
+                this.style.visibility = "hidden";
+            })
+    }
+
+    wrapper[0].children[posicion].addEventListener("click",function (){
+        alert("Ganaste!");
+        window.location.reload(false);
+    })
+}
+
+
+function setearColoresCuadrados(dificultad,stringColor){
+    var wrapper = document.getElementsByClassName("wrapper")
+    if(dificultad=="facil"){
+        var valorFacil = generarValorAleatorio(3);
+        if(valorFacil>=1){
+            valorFacil=valorFacil-1;
+        }
+        wrapper[0].children[0].style.backgroundColor= generarColorAleatorio();
+        wrapper[0].children[1].style.backgroundColor = generarColorAleatorio();
+        wrapper[0].children[2].style.backgroundColor = generarColorAleatorio();
+        console.log(valorFacil);
+        wrapper[0].children[valorFacil].style.backgroundColor = stringColor;
+       
+        setearEventoIncorrecto(valorFacil,dificultad);
+    }
+    else{
+        var valorDificil = generarValorAleatorio(6);
+        if(valorDificil>=1){
+            valorDificil=valorDificil-1;
+        }
+        wrapper[0].children[0].style.backgroundColor= generarColorAleatorio();
+        wrapper[0].children[1].style.backgroundColor = generarColorAleatorio();
+        wrapper[0].children[2].style.backgroundColor = generarColorAleatorio();
+        wrapper[0].children[3].style.backgroundColor = generarColorAleatorio();
+        wrapper[0].children[4].style.backgroundColor = generarColorAleatorio();
+        wrapper[0].children[5].style.backgroundColor = generarColorAleatorio();
+        console.log(valorDificil);
+        wrapper[0].children[valorDificil].style.backgroundColor = stringColor;
+        
+        setearEventoIncorrecto(valorDificil,dificultad);
+    }
+}
+
+function setearBotonesDificultad(){
+    var facil = document.getElementById("facil");
+    var dificil = document.getElementById("dificil");
+
+    facil.addEventListener("click", function () {
+        mostrarSegunDificultad("facil");
+    })
+
+    dificil.addEventListener("click", function () {
+        mostrarSegunDificultad("dificil");
+    })
+}
+
+
+function setearColorTexto(stringColor){
+    var p = document.getElementById("color")
+    p.textContent = stringColor.toLocaleUpperCase(); 
+}
+
+function generarColorAleatorio(){
+    var vectorColores = []
+    for(var i = 0;i<3;i++){
+        vectorColores[i] = colorAleatorio()
+    }
+    var stringColor = "rgb( "+vectorColores[0]+" , "+vectorColores[1]+" , "+vectorColores[2]+" )"
+    return stringColor
+}
+
+function colorAleatorio(){
+    var color =  Math.floor(Math.random() * (255 - 0)) + 0;
+    return color.toString();
+}
+
+
+//------------------------------
+
+
+
+/*
+var stringColor = generarColorAleatorio();
+var condicionInicial = false;
 function reiniciar(){
     window.location.reload(false);
 }
 
+
+
 function cargarColorTexto(){
     var stringColor = generarColorAleatorio()
-
-
         iniciarBotones(stringColor);
-    
-    
 }
 
-var p = document.getElementById("color")
+
 
 
 function chequearBotones(){
@@ -31,22 +164,14 @@ function chequearBotones(){
         facil.style.backgroundColor = "plum"
         facil.style.color = "white"
     }
+
+    setearColorTexto(color);
 }
 
-function generarColorAleatorio(){
-    var vectorColores = []
-    for(var i = 0;i<3;i++){
-        vectorColores[i] = colorAleatorio()
-    }
-    var stringColor = "rgb( "+vectorColores[0]+" , "+vectorColores[1]+" , "+vectorColores[2]+" )"
-    
-    return stringColor
-}
 
-function colorAleatorio(){
-    var color =  Math.floor(Math.random() * (255 - 0)) + 0;
-    return color.toString();
-}
+
+
+
 
 function iniciarBotones(stringColor){
     var facil = document.getElementById("facil")
@@ -55,12 +180,14 @@ function iniciarBotones(stringColor){
 
     facil.addEventListener("click",function (){
         document.getElementById("div1").style.backgroundColor="black"
-        document.getElementById("dificil").addEventListener("click",function(){ reiniciar();})
+        
+        condicionInicial = true;
         mostrarFacil(stringColor);
     })
     dificil.addEventListener("click",function (){
         document.getElementById("div1").style.backgroundColor="black"
-        document.getElementById("facil").addEventListener("click",function(){ reiniciar();})
+        
+        condicionInicial = true;
         mostrarDificil(stringColor);
     })
 }
@@ -76,7 +203,7 @@ function mostrarFacil(stringColor){
 
    // chequearBotones();
      
-     p.textContent = stringColor.toLocaleUpperCase(); 
+     
     setearBotones(stringColor,"facil");
 }
 
@@ -189,6 +316,5 @@ function ocultarBotones(){
     wrapper[0].children[5].style.visibility = "hidden";
 }
 
-
-ocultarBotones();
-cargarColorTexto();
+*/
+setearBotonesDificultad();
